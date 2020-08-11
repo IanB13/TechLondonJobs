56b8e87b434598fe0226044f9667757748d0ec8d
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-08-11 00:49:51 -0600

    finished geting Ids

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const puppeteer = require('puppeteer');
 const getJobPostingIds = async () => {
     const browser = await puppeteer.launch({
         headless: true , 
-        defaultViewport: {width: 1920, height: 1080} //for larger screen shots 
+        defaultViewport: {width: 1920, height: 2000} //for larger screen shots 
         });
-    
 
     const page = await browser.newPage();
     
@@ -17,28 +16,33 @@ const getJobPostingIds = async () => {
 
     await page.click('body > div.fade.modal-global.modal.show > div > div > div.modal-header > button > span:nth-child(1)')//gets rid of start bit
 
-
     await page.waitFor(1000)
     
-    const elementsList = await page.$$(".list-startups-main") //gets node list of all jobs on page
+
+    const completeIdList = []
+    //loop, currently just does 5
+    for (let i = 0; i < 5; i++) {
+        await page.waitFor(1000) //waits for 
+        const elementsList = await page.$$(".list-startups-main") //gets node list of all jobs on page
     
-    const idList = elementsList.map( element =>{
-        const descriptionString = element._remoteObject.description;
-        const id = descriptionString.slice( descriptionString.indexOf('#')+1, descriptionString.indexOf('.'));
-        return id
-    }) // maps node list to Id Strings
-    // IDs can be used to check individual job postings in URL 
-
-    console.log(idList)
-
-    await page.screenshot({path: 'c.png'});
+        const idList = elementsList.map( element =>{
+            const descriptionString = element._remoteObject.description;
+            const id = descriptionString.slice( descriptionString.indexOf('#')+1, descriptionString.indexOf('.'));
+            return id
+        }) // maps node list to Id Strings
+        
+        // IDs can be used to check individual job postings in URL 
     
+    completeIdList.push(...idList)
+       //await page.screenshot({path: `${i}.png`}); //for screenshots of each individual page
+       await page.click('#root > div:nth-child(7) > div > div.list-discovery-space.col-lg-7.col-md-12.col-sm-12.col-12 > div.pagination-margin > div > ul > li.rc-pagination-next > button > img')
+        
+    }
+
     
-
-
     await browser.close()
-    console.log("Finished!")
-
+    console.log(completeIdList)
+    return(completeIdList)
 }
 
-getJobPostingIds()
+console.log(getJobPostingIds())
