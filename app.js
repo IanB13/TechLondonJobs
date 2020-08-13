60bc1ae7049e96248fa1a391dfe915b9d2729140
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-08-12 22:14:31 -0600

    PORT

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const cors = require('cors');
 const jobLinkRouter = require('./controllers/jobLinks')
@@ -18,6 +18,6 @@ app.use(express.static('build'))
 
 
 // set port, listen for requests
-app.listen(port, () => {
-  console.log("Server is running on port 4000.");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
