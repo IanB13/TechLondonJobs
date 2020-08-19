637260f341881c88b74261a94f4dcad5c17a6675
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-08-19 15:32:48 -0600

    Working mongoose functions

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,9 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
 
-const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {useNewUrlParser: true , useUnifiedTopology: true});
+
 const JobPosting = require('../models/JobPosting')
 
 mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true  }).then(success =>{
@@ -15,30 +11,21 @@ mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true  }).then(
   
 
 const getJobLinks = async ()=> {
-    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-    await client.connect()
-    const cursor = client.db("TCVjob").collection("joblinks").find()
-    const jobLinks = await cursor.toArray() 
-    await client.close();
-    return jobLinks;
+    const jobPostings =  await JobPosting.find({})
+    return jobPostings
 }
 
 const getJobLink = async (ID) =>{
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  })
-    await client.connect()
-    console.log("Id is",ID)
-    const cursor = client.db("TCVjob").collection("joblinks").find({jobID:ID})
-    const jobLink = await cursor.toArray() 
-    console.log(jobLink)
-    return jobLink;
+    const jobPosting =  await JobPosting.find({jobID:ID});
+    return jobPosting;
 
 }
 
 const updateJobLinks = async (jobLinks) => {
     console.log("update Job Links")
    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  })
-    JobPosting.deleteMany()
-    JobPosting.insertMany(jobLinks)
+    await JobPosting.deleteMany()
+    await JobPosting.insertMany(jobLinks)
 }
 
 module.exports = {
